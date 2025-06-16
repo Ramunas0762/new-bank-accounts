94026cfc8a6849d1cff68a1f93ef5e2ff181d404
Ramunas <rojus@msn.com>
2025-06-16 14:09:11 +0300

    new_bank_accounts.py

--- main.py
+++ main.py
@@ -5,15 +5,43 @@ app = FastAPI()
 
 class Account(BaseModel):
     id: int
-    name: str
-    surname:str
+    type: str
+    person_name:str
+    person_address: str
 
-# type hint for a list of accounts
-accounts:list[Account] = []
+def write_account_to_file(account: "Account"):
+        with open("account.txt", "a") as file:
+            file.write(f"{account.id}, {account.type}, {account.person_name}, {account.person_address}\n")
+
+def read_accounts_from_file():
+    accounts = []
+    with open("account.txt", "r") as file:
+            for line in file:
+                id, type, person_name, person_address = line.strip().split(", ")
+                accounts.append(Account(id=int(id), type=type, person_name=person_name, person_address=person_address))
+    
+    return accounts
+
+def delete_account_from_file(account_id_to_delete: int):
+    accounts = read_accounts_from_file()
+    accounts = [account for account in accounts if account.id != account_id_to_delete]
+    
+    with open("account.txt", "w") as file:
+        for account in accounts:
+            file.write(f"{account.id}, {account.type}, {account.person_name}, {account.person_address}\n")
+
+
+import os
+if not os.path.exists("account.txt"):
+     open("account.txt", "w").close()
+
+
+accounts:list[Account] = read_accounts_from_file()
 
 @app.post("/accounts/")
 def create_account(account: Account):
     accounts.append(account)
+    write_account_to_file(account)
     return {"message": "Account created successfully"}
 
 @app.get("/accounts/")
@@ -31,4 +59,5 @@ def get_account(account_id: int):
 @app.delete("/accounts/{account_id}")
 def delete_account(account_id: int):
     accounts[:] = [account for account in accounts if account.id != account_id]
+    delete_account_from_file(account_id)
     return {"message": "Account deleted successfully"}
